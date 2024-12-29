4ff6e4f94bf1a8d0e27961e1728839fcd8cf1ba7
Olga <olyska20@yandex.ru>
2024-12-30 01:09:19 +0300

    add topic

--- src/main/java/org/example/SimpleProducer.java
+++ src/main/java/org/example/SimpleProducer.java
@@ -1,10 +1,17 @@
 package org.example;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class SimpleProducer {
     public static void main(String[] args) {
+        createTopic();
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
@@ -19,4 +26,18 @@ public class SimpleProducer {
 
         producer.close();
     }
+
+    private static void createTopic(){
+        Properties config = new Properties();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        try (AdminClient adminClient = AdminClient.create(config)) {
+            NewTopic newTopic = new NewTopic("my-topic", 1, (short) 1);
+            adminClient.createTopics(Collections.singleton(newTopic)).all().get();
+            System.out.println("Топик успешно создан.");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
