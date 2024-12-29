388c8222651c74f5cc8d02ff880100b69fc9b940
Olga <olyska20@yandex.ru>
2024-12-30 00:49:24 +0300

    add yml file for start kafka + zookeeper

---
+++ src/main/java/org/example/SimpleProducer.java
@@ -0,0 +1,22 @@
+package org.example;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("test-topic", Integer.toString(i), "Message " + i);
+            producer.send(record);
+        }
+
+        producer.close();
+    }
+}
